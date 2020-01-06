20a74b6cb3753320098aaff82d7692f1a6620739
Mike-Kaydalov <mike.kaydalov@gmail.com>
2020-01-06 20:03:20 +0200

    just

---
+++ search.py
@@ -0,0 +1,24 @@
+print('Введите число ' )
+n=0
+k=0
+l=0
+o=0
+j=0
+n=float(input())
+l=n
+print(l)
+o=n-2
+
+while k<o  :
+    l=l-1
+    print(str(l)+"llllll")
+    print(n)
+    j=n*l
+    print(str(j)+'gghhghhg')
+    k+=1
+    print(k)
+    print('gh')
+    print(l)
+
+
+print(j)
---
+++ venv/!.py
@@ -0,0 +1,16 @@
+print('введите число')
+n=float(input())
+i=0
+l=n-1
+k=0
+k=n
+while i < n-2:
+    k=k*l
+    print(k)
+    l-=1
+    i+=1
+
+
+
+
+print(k)
